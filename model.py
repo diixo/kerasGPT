268b7fb6317dc272048953a0270133de1610305c
diixo <anatolii.podoba@gmail.com>
2024-10-17 07:51:43 +0300

    def generate(..) draft

--- model.py
+++ model.py
@@ -2,13 +2,16 @@
 import math
 import tensorflow as tf
 import keras
+import keras.backend as K
 from keras import layers
 from dataclasses import dataclass
 from dynamic_dict import DynamicDict
 
 
 
-""" layer: LayerNorm with optional bias"""
+""" 
+## layer: LayerNorm with optional bias
+"""
 class LayerNorm():
 
     def __init__(self, ndim, use_bias=True):
@@ -52,6 +55,7 @@ class MLP():
         return x
 
     
+    # special scaled init to the residual projections, per GPT-2 paper
     def init_weights(self):
         if isinstance(self.fc, keras.layers.Dense):
             initializer = keras.initializers.RandomNormal(mean=0.0, stddev=0.02)
@@ -74,7 +78,7 @@ class MLP():
 
 
 """
-## Implement a Transformer block as a layer
+## Implement TransformerBlock
 """
 class Block():
 
@@ -98,7 +102,7 @@ class Block():
 @dataclass
 class GPTConfig:
     block_size: int = 512
-    vocab_size: int = 50304 # GPT-2 vocab_size of 50257, padded up to nearest multiple of 64 for efficiency
+    vocab_size: int = 50304 # GPT-2 vocab_size of 50257, padded up to nearest multiple of 64 (and 48) for efficiency
     n_layer: int = 4
     n_head: int = 4
     n_embd: int = 128
@@ -106,10 +110,13 @@ class GPTConfig:
     bias: bool = True   # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
 
 
-class GPT:
+"""
+## Build the GPT model class as a `keras.Model` subclass
+"""
+class GPTModel(keras.Model):
 
     def __init__(self, config):
-
+        super().__init__()
         assert config.vocab_size is not None
         assert config.block_size is not None
         self.config = config
@@ -132,7 +139,7 @@ class GPT:
 
     def call(self, idx, targets=None):
 
-        b, t = tf.shape(idx)[0], tf.shape(idx)[1]
+        b, t = tf.shape(idx)[0], tf.shape(idx)[1] # shape(batch_size, sequence_length)
         assert t <= self.config.block_size, f"Cannot forward sequence of length {t}, block size is only {self.config.block_size}"
         
         pos = tf.range(0, t, dtype=tf.int32)    # shape (t)
@@ -154,3 +161,37 @@ class GPT:
             loss = None
 
         return logits, loss
+
+
+    """
+    Generate indexed based sequence : idx (Tensor, shape (b, t)).
+    """
+    @tf.function # create function as executive graph
+    def generate(self, idx, max_new_tokens, temperature=1.0, do_sample=False, top_k=None):
+        for _ in range(max_new_tokens):
+            idx_cond = idx if idx.shape[1] <= self.block_size else idx[:, -self.block_size:]
+
+            # use without gradients
+            with tf.no_grad():
+                logits, _ = self(idx_cond)
+
+            logits = logits[:, -1, :] / temperature
+            
+            if top_k is not None:
+                v, _ = tf.math.top_k(logits, top_k)
+                logits = tf.where(logits < v[:, -1:], -float('Inf'), logits)
+
+            probs = tf.nn.softmax(logits, axis=-1)
+
+            if do_sample:
+                idx_next = tf.random.categorical(probs, num_samples=1)
+            else:
+                _, idx_next = tf.math.top_k(probs, k=1, sorted=False)
+
+            idx = tf.concat([idx, idx_next], axis=1)
+
+        return idx
+
+
+    def get_num_embeddings(self):
+        return round(1 / 1000) / 1000
